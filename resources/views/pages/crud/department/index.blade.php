de07c9907e2a2e55fdd351c6fb1493ec5204112d
febryntara <paglutgede@gmail.com>
2023-07-09 12:59:08 +0800

    remove some details

--- resources/views/pages/crud/department/index.blade.php
+++ resources/views/pages/crud/department/index.blade.php
@@ -7,13 +7,6 @@
     <div class="grid grid-cols-12 gap-6 mt-5">
         <div class="intro-y col-span-12 flex flex-wrap sm:flex-nowrap items-center mt-2">
             <a href="{{ route('department.create') }}" class="btn btn-primary shadow-md mr-2">Tambah Department Baru</a>
-            <div class="hidden md:block mx-auto text-slate-500">Showing 1 to 10 of 150 entries</div>
-            <div class="w-full sm:w-auto mt-3 sm:mt-0 sm:ml-auto md:ml-0">
-                <div class="w-56 relative text-slate-500">
-                    <input type="text" class="form-control w-56 box pr-10" placeholder="Search...">
-                    <i class="w-4 h-4 absolute my-auto inset-y-0 mr-3 right-0" data-lucide="search"></i>
-                </div>
-            </div>
         </div>
         <!-- BEGIN: Data List -->
         <div class="intro-y col-span-12 overflow-auto lg:overflow-visible">
